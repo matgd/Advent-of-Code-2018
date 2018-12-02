daedf8c735447faf223e0c9ac0557e56e4309ac5
matgd <matgd@users.noreply.github.com>
2018-12-02 13:20:13 +0100

    Day 2

---
+++ day_2/advent_2018y_2d.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+
+my $_2char_words = 0;
+my $_3char_words = 0;
+
+open(DATA, "<input.txt") or die "Couldn't open the file, $!";
+
+while(<DATA>) {
+    chomp $_;
+    my @line_chars = split //, $_;
+    my %chars_hash = map { $_ => 0} @line_chars;
+
+    foreach my $char (@line_chars) {
+	$chars_hash{$char}++;
+    }	
+    
+    print "hash: ", %chars_hash, "\n";    
+
+    my %counts = map { $_ => 1 } values %chars_hash;
+    print "counts: ", %counts, "\n";
+    
+    foreach my $count (keys %counts) {
+	if($count == 2) {
+	    $_2char_words++;
+	} elsif($count == 3) {
+	    $_3char_words++;
+	}
+    }
+}
+
+print "2-char words: ", $_2char_words, "\n";
+print "3-char words: ", $_3char_words, "\n";
+print "Checksum: ", $_2char_words * $_3char_words, "\n";
